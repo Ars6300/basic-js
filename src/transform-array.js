596e2a0cb9792aa017fbce23cebb764de10774df
ars6300 <ars6300@live.com>
2020-02-25 01:18:26 +0500

    feat: add solution transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,23 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(!Array.isArray(arr)) throw new Error;
+    const result = [];
+    for(let item = 0; item < arr.length; item++){
+        switch(arr[item]){
+            case '--double-next':
+                if(item < arr.length-1) result.push(arr[item + 1]);
+                break;
+            case '--double-prev':
+                if(item>0) result.push(arr[item - 1]);
+                break;
+            case '--discard-next':
+                if(item < arr.length-1) item++;
+                break;
+            case '--discard-prev':
+                if(arr.length > 0) result.pop();
+                break;
+            default:
+                result.push(arr[item])
+        }
+    }
+    return result;
 };
