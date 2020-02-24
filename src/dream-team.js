530bb440e8525472fe7dc15e7fac0a738b02cfa6
ars6300 <ars6300@live.com>
2020-02-24 16:01:59 +0500

    feat: add solution dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,10 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam( members ) {
+  if(!Array.isArray(members)){return false}
+  let result = [];
+  for (item in members){
+    if(typeof members[item] == 'string'){
+      result.push(members[item].trim().slice(0, 1).toUpperCase());
+    }
+  }
+  return result.sort().join('');
 };
