f5e2fd4f4730b50480475672355f1a8244ae8f87
ars6300 <ars6300@live.com>
2020-02-26 20:22:35 +0500

    feat: add solution carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,7 +2,11 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-    throw 'Not implemented';
+    if(typeof sampleActivity != 'string'){ return false }
+    let activity = parseFloat(sampleActivity);
+    
+    if(activity > 15 || activity <= 0 || isNaN(activity)) return false;
 
+     return Math.ceil( Math.log(MODERN_ACTIVITY/activity)/(0.693/HALF_LIFE_PERIOD))
   
 };
