3131c02d2c36d718a25d483d5e955975c99db4f0
ars6300 <ars6300@live.com>
2020-03-12 21:59:14 +0500

    feat: one more left

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,31 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(direct){
+        this.direct = direct;
+    }
+    encrypt(message, key) {
+        if(!message || !key){ throw new Error};
+        let regex = /[A-Z]/;
+        let arrayMessage = this.direct == false ? message.toUpperCase().split('').reverse() : message.toUpperCase().split('');
+        let arrayForKey = key.toUpperCase().split('');
+        let arrayKey = [];
+        for(let c = 0; c < arrayMessage.length; c++){
+          if(arrayForKey[c] && regex.test(arrayMessage[c])){
+          arrayKey.push(arrayForKey[c])
+          }
+          else if(regex.test(arrayMessage[c])){
+          arrayKey.push(arrayForKey[c - arrayForKey.length])
+          }
+          else{
+          arrayKey.push(' ');
+          
+          }
+        }
+        return arrayKey;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(encryptedMessage, key) {
+        if(!encryptedMessage || !key){ throw new Error};
+
     }
 }
 
